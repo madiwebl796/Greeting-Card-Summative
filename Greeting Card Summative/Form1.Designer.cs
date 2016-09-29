8c8001459c20d6f8be38cccddefcb0f6505c3e50
madiwebl796 <madiwebl796@ed.amdsb.ca>
2016-09-29 13:34:51 -0400

    Add project files.

---
+++ Greeting Card Summative/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Greeting_Card_Summative
+{
+    partial class greetingCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openCard = new System.Windows.Forms.Button();
+            this.openLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openCard
+            // 
+            this.openCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openCard.Location = new System.Drawing.Point(48, 21);
+            this.openCard.Name = "openCard";
+            this.openCard.Size = new System.Drawing.Size(190, 95);
+            this.openCard.TabIndex = 0;
+            this.openCard.Text = "Open Greeting Card";
+            this.openCard.UseVisualStyleBackColor = true;
+            this.openCard.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openLabel
+            // 
+            this.openLabel.AutoSize = true;
+            this.openLabel.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openLabel.Location = new System.Drawing.Point(24, 184);
+            this.openLabel.Name = "openLabel";
+            this.openLabel.Size = new System.Drawing.Size(248, 30);
+            this.openLabel.TabIndex = 1;
+            this.openLabel.Text = "What\'s your destiny???";
+            // 
+            // greetingCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.openLabel);
+            this.Controls.Add(this.openCard);
+            this.Name = "greetingCard";
+            this.Text = "Greeting Card";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openCard;
+        private System.Windows.Forms.Label openLabel;
+    }
+}
+
---
+++ Greeting Card Summative/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greeting_Card_Summative
+{
+    public partial class greetingCard : Form
+    {
+        public greetingCard()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics FormGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.White, 10);
+            SolidBrush WhiteBrush = new SolidBrush(Color.White);
+            openCard.Hide();
+            openLabel.Hide();
+
+            Pen purplePen = new Pen(Color.Purple, 05);
+
+            FormGraphics.Clear(Color.Black);
+
+            FormGraphics.FillEllipse(WhiteBrush, 161, 22, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 163, 56, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 153, 83, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 126, 89, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 89, 78, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 157, 124, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 175, 150, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 149, 192, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 126, 136, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 118, 214, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 102, 157, 07, 07);
+            FormGraphics.FillEllipse(WhiteBrush, 77, 196, 07, 07);
+
+            FormGraphics.DrawLine(purplePen, 164, 22, 167, 58);
+            FormGraphics.DrawLine(purplePen, 167, 56, 157, 85);
+            FormGraphics.DrawLine(purplePen, 157, 85, 130, 89);
+            FormGraphics.DrawLine(purplePen, 130, 89, 92, 78);
+            FormGraphics.DrawLine(purplePen, 161, 124, 179, 150);
+            FormGraphics.DrawLine(purplePen, 179, 150, 153, 192);
+
+            //FormGraphics.DrawLine(purplePen, 130, 136, 122, 214);
+            //FormGraphics.DrawLine(purplePen, 122, 214, 106, 157);
+            FormGraphics.DrawLine(purplePen, 106, 157, 81, 196);
+
+            FormGraphics.DrawLine(purplePen, 157, 85, 161, 124);
+           // FormGraphics.DrawLine(purplePen, 130, 89, 122, 214);
+          
+            FormGraphics.DrawLine(purplePen, 179, 150, 130, 136);
+            FormGraphics.DrawLine(purplePen, 122, 214, 153, 192);
+            FormGraphics.DrawLine(purplePen, 81, 196, 106, 157);
+            FormGraphics.DrawLine(purplePen, 130, 136, 130, 89);
+            FormGraphics.DrawLine(purplePen, 130, 136, 81, 196);
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
